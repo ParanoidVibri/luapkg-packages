77c9148f9a97a726a2205a7276158cacc3bce854
ParanoidVibri <165101870+ParanoidVibri@users.noreply.github.com>
2024-06-28 01:37:19 +0300

    ported tinyfetch

---
+++ tinyfetch.lua
@@ -0,0 +1,9 @@
+meta = {
+    ["tinyfetch"] = {
+        version = "1.0.0",
+        description = "fetch program written in pure C",
+        author = "kernaltrap8",
+        url = "https://github.com/kernaltrap8/tinyfetch",
+        license = "GPL 3.0",
+    },
+}
